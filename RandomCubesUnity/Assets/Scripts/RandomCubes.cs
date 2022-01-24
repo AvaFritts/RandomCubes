032e291b5c6fb5927e85b5932c9591201222dd95
AvaFritts <avanfritts@gmail.com>
2022-01-24 15:11:35 -0600

    Added Scripts

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by: Ava Fritts
+ * Date Created: Jan 24th, 2022 (last day if being 19)
+ * 
+ * Last Edited by: N/A
+ * Last Edited: Jan 24th, 2022
+ * 
+ * Description: Spawn multiple cube prefabs into the scene.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
